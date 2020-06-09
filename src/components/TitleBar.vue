f2cb609e10e7550c6154507d137e49c75c9f8a72
mooen <1762909091@qq.com>
2020-06-09 22:55:45 +0800

    -- init

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="title-bar"></div>
+</template>
+
+<script>
+    export default {
+        name: "TitleBar"
+    }
+</script>
+
+
+<style scoped>
+
+    .title-bar{
+        background-color: pink;
+    }
+
+
+</style>
---
+++ src/view/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Main"
+    }
+</script>
+
+<style scoped>
+
+    .main{
+        height: 100vh;
+    }
+
+</style>
